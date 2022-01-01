4b166fc30fd95861768f454aa5eb6a40e4301949
TheSnoozer <s.lange@alumni.tu-berlin.de>
2022-01-01 12:36:35 +0100

    Make the SeablockPlanningTools work with Factorio 1.1
    Effectively the consuming type can now only be "none" or "game-only" (https://lua-api.factorio.com/latest/LuaCustomInputPrototype.html)

--- src/data.lua
+++ src/data.lua
@@ -3,37 +3,37 @@ data:extend {
     type = "custom-input",
     name = "spt-toggle-inserter-speed",
     key_sequence = "ALT + SPACE",
-    consuming = "script-only"
+    consuming = "none"
   },
   {
     type = "custom-input",
     name = "spt-optimize-default-range",
     key_sequence = "ALT + O",
-    consuming = "script-only"
+    consuming = "none"
   },
   {
     type = "custom-input",
     name = "spt-optimize-small-range",
     key_sequence = "CONTROL + ALT + O",
-    consuming = "script-only"
+    consuming = "none"
   },
   {
     type = "custom-input",
     name = "spt-optimize-large-range",
     key_sequence = "SHIFT + ALT + O",
-    consuming = "script-only"
+    consuming = "none"
   },
   {
     type = "custom-input",
     name = "spt-show-chest-throughput",
     key_sequence = "ALT + T",
-    consuming = "script-only"
+    consuming = "none"
   },
 
   {
     type = "custom-input",
     name = "spt-show-nests",
     key_sequence = "ALT + N",
-    consuming = "script-only"
+    consuming = "none"
   }
 }
